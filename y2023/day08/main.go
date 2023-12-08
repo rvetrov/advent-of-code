534922d86dc1aec88cca9ea2f321c9c490c1e837
r-vetrov <r-vetrov@yandex-team.ru>
2023-12-09 02:53:09 +0400

    Day 8 solved

---
+++ y2023/day08/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"adventofcode.com/2023/internal/math"
+	"adventofcode.com/2023/internal/utils"
+)
+
+type Crossroad struct {
+	Left  string
+	Right string
+}
+
+type Map struct {
+	Route string
+	Edges map[string]Crossroad
+}
+
+type MileStone struct {
+	Pos  string
+	Step int
+}
+
+type Route []MileStone
+
+func (m Map) FindLoop(position string) (int, Route) {
+	visited := map[string]int{}
+
+	totalRouteSteps := 0
+	stateStr := fmt.Sprintf("%s,%d", position, totalRouteSteps)
+	visited[stateStr] = totalRouteSteps
+	route := Route{{stateStr, totalRouteSteps}}
+	loopSize := 0
+
+	for {
+		routeStep := totalRouteSteps % len(m.Route)
+		if m.Route[routeStep] == 'L' {
+			position = m.Edges[position].Left
+		} else {
+			position = m.Edges[position].Right
+		}
+		totalRouteSteps++
+
+		stateStr = fmt.Sprintf("%s,%d", position, totalRouteSteps%len(m.Route))
+		if stateStr[2] == 'Z' {
+			route = append(route, MileStone{stateStr, totalRouteSteps})
+			if visited[stateStr] > 0 {
+				loopSize = totalRouteSteps - visited[stateStr]
+				break
+			}
+			visited[stateStr] = totalRouteSteps
+		}
+	}
+	return loopSize, route
+}
+
+func newMap(input string) Map {
+	lines := []string{}
+	for _, line := range strings.Split(input, "\n") {
+		line = strings.TrimSpace(line)
+		if len(line) > 0 {
+			lines = append(lines, line)
+		}
+	}
+
+	edges := map[string]Crossroad{}
+	for i := 1; i < len(lines); i++ {
+		var from, left, right string
+		if n, err := fmt.Sscanf(lines[i], "%3s = (%3s, %3s)", &from, &left, &right); err == nil {
+			edges[from] = Crossroad{left, right}
+		} else {
+			fmt.Println(n, err)
+		}
+	}
+	return Map{Route: lines[0], Edges: edges}
+}
+
+func solveV1(input string) int {
+	mp := newMap(input)
+	res := 0
+	for pos, cur := 0, "AAA"; cur != "ZZZ"; pos = (pos + 1) % len(mp.Route) {
+		if mp.Route[pos] == 'L' {
+			cur = mp.Edges[cur].Left
+		} else {
+			cur = mp.Edges[cur].Right
+		}
+		res++
+	}
+	return res
+}
+
+func solveV2(input string) int {
+	mp := newMap(input)
+	ghosts := []string{}
+	for from := range mp.Edges {
+		if from[2] == 'A' {
+			ghosts = append(ghosts, from)
+		}
+	}
+	fmt.Println("Ghosts:", len(ghosts))
+
+	res := 1
+	for _, ghost := range ghosts {
+		loopSize, route := mp.FindLoop(ghost)
+		fmt.Println(ghost, loopSize, route)
+		// Prints the following for input.big:
+		// XCA 19099 [{XCA,0 0} {NNZ,0 19099} {NNZ,0 38198}]
+		// AAA 19637 [{AAA,0 0} {ZZZ,0 19637} {ZZZ,0 39274}]
+		// QGA 14257 [{QGA,0 0} {GHZ,0 14257} {GHZ,0 28514}]
+		// LBA 11567 [{LBA,0 0} {NPZ,0 11567} {NPZ,0 23134}]
+		// GSA 12643 [{GSA,0 0} {SPZ,0 12643} {SPZ,0 25286}]
+		// LHA 15871 [{LHA,0 0} {HVZ,0 15871} {HVZ,0 31742}]
+
+		// Every route is a loop with only one target position. Therefore, LCM works.
+		res = math.LCM(res, loopSize)
+	}
+	return res
+}
+
+func main() {
+	input := utils.MustReadInput("input.big")
+	res := solveV2(input)
+	utils.MustWriteOutput("output-v2.big", fmt.Sprint(res))
+}
---
+++ y2023/day08/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testCase1 = `
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+`
+
+const testCase2 = `
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+`
+
+const testCaseV2 = `
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+`
+
+func TestV1(t *testing.T) {
+	res := solveV1(testCase1)
+	require.Equal(t, 2, res)
+
+	res = solveV1(testCase2)
+	require.Equal(t, 6, res)
+}
+
+func TestV2(t *testing.T) {
+	res := solveV2(testCaseV2)
+	require.Equal(t, 6, res)
+}
