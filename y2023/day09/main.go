0537d6cf25c8756a4ea760c20a3095dfd8cba008
r-vetrov <r-vetrov@yandex-team.ru>
2023-12-09 15:15:16 +0400

    Day 9 solved

---
+++ y2023/day09/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"adventofcode.com/internal/utils"
+)
+
+type History []int
+
+func (h History) allZeroes() bool {
+	for _, num := range h {
+		if num != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (h History) predictNext() int {
+	if h.allZeroes() {
+		return 0
+	}
+	nextH := make(History, len(h)-1)
+	for j := 0; j+1 < len(h); j++ {
+		nextH[j] = h[j+1] - h[j]
+	}
+	return nextH.predictNext() + h[len(h)-1]
+}
+
+func (h History) predictPrev() int {
+	if h.allZeroes() {
+		return 0
+	}
+	nextH := make(History, len(h)-1)
+	for j := 0; j+1 < len(h); j++ {
+		nextH[j] = h[j+1] - h[j]
+	}
+	return h[0] - nextH.predictPrev()
+}
+
+func parseHistories(input string) []History {
+	res := []History{}
+	for _, line := range strings.Split(input, "\n") {
+		if line = strings.TrimSpace(line); len(line) > 0 {
+			history := History{}
+			for _, numStr := range strings.Split(line, " ") {
+				if num, err := strconv.Atoi(numStr); err == nil {
+					history = append(history, num)
+				} else {
+					log.Fatalf("Failed to parse %q: %q", numStr, err)
+				}
+			}
+			res = append(res, history)
+		}
+	}
+	return res
+}
+
+func solveV1(input string) int {
+	hs := parseHistories(input)
+	res := 0
+	for _, history := range hs {
+		res += history.predictNext()
+	}
+	return res
+}
+
+func solveV2(input string) int {
+	hs := parseHistories(input)
+	res := 0
+	for _, history := range hs {
+		res += history.predictPrev()
+	}
+	return res
+}
+
+func main() {
+	input := utils.MustReadInput("input.big")
+	res := solveV2(input)
+	utils.MustWriteOutput("output-v2.big", fmt.Sprint(res))
+}
---
+++ y2023/day09/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testCase1 = `
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+`
+
+func TestV1(t *testing.T) {
+	res := solveV1(testCase1)
+	require.Equal(t, 114, res)
+}
+
+func TestV2(t *testing.T) {
+	res := solveV2(testCase1)
+	require.Equal(t, 2, res)
+}
