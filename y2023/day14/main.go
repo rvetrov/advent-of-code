bc6ecc6cc7f48f2f3e0d14339f61a3ce406f6d5b
Roman <ravetrov@gmail.com>
2023-12-15 03:52:25 +0400

    Solved Day 14

---
+++ y2023/day14/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"adventofcode.com/internal/utils"
+)
+
+func tiltLeft(line string) string {
+	builder := strings.Builder{}
+	j := 0
+	bs := strings.Split(line, "")
+	for i := 0; i < len(bs); i++ {
+		if j <= i {
+			j = i + 1
+		}
+		if bs[i] == "." {
+			for j < len(bs) && bs[j] == "." {
+				j++
+			}
+			if j < len(bs) && bs[j] == "O" {
+				bs[i], bs[j] = bs[j], bs[i]
+			}
+		}
+		builder.WriteString(bs[i])
+	}
+	return builder.String()
+}
+
+func weighLine(line string) (res int) {
+	for i, ch := range line {
+		if ch == 'O' {
+			res += len(line) - i
+		}
+	}
+	return res
+}
+
+func solveV1(input string) int {
+	field := utils.RotateCCW(utils.Lines(input))
+	fmt.Println(field)
+	res := 0
+	for _, line := range field {
+		res += weighLine(tiltLeft(line))
+	}
+	return res
+}
+
+func cycleTilts(field []string) []string {
+	for rotNum := 0; rotNum < 4; rotNum++ {
+		for i, line := range field {
+			field[i] = tiltLeft(line)
+		}
+		field = utils.RotateCW(field)
+	}
+	return field
+}
+
+func solveV2(input string) int {
+	field := utils.RotateCCW(utils.Lines(input))
+	cache := map[string]int{}
+
+	totalRotations := 1000000000
+	for step := 0; step < totalRotations; step++ {
+		fingerprint := fmt.Sprintf("%d-%s", step%4, strings.Join(field, ""))
+		if prevStep, ok := cache[fingerprint]; ok {
+			cycleLen := step - prevStep
+			step += cycleLen * ((totalRotations - step) / cycleLen)
+		}
+		cache[fingerprint] = step
+
+		field = cycleTilts(field)
+	}
+
+	res := 0
+	for _, line := range field {
+		res += weighLine(line)
+	}
+	return res
+}
+
+func main() {
+	input := utils.MustReadInput("input.big")
+	res := solveV2(input)
+	utils.MustWriteOutput("output-v2.big", fmt.Sprint(res))
+}
---
+++ y2023/day14/main_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"testing"
+
+	"adventofcode.com/internal/utils"
+	"github.com/stretchr/testify/require"
+)
+
+const testCase1 = `
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+`
+
+const expectedAfterCycles1 = `
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+`
+
+func TestV1(t *testing.T) {
+	res := solveV1(testCase1)
+	require.Equal(t, 136, res)
+}
+
+func TestV2(t *testing.T) {
+	field := utils.RotateCCW(utils.Lines(testCase1))
+	for _, expected := range utils.SplitByEmptyLine(expectedAfterCycles1) {
+		field = cycleTilts(field)
+
+		actualField := utils.RotateCW(field)
+		require.Equal(t, expected, actualField)
+	}
+
+	res := solveV2(testCase1)
+	require.Equal(t, 64, res)
+}
