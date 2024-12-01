5ff8ca9364b78e92d4a4c0d780a88f8f8bd83d6e
Roman <ravetrov@gmail.com>
2024-12-01 23:27:14 +0400

    Init for y2024: add not added

---
+++ y2023/day22/main.go
@@ -0,0 +1,10 @@
+package day22
+
+func SolveV1(input string) int {
+	res := 0
+	return res
+}
+
+func SolveV2(input string) int {
+	return SolveV1(input)
+}
---
+++ y2023/day22/main_test.go
@@ -0,0 +1,31 @@
+package day22
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testCase1 = `
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+`
+
+func TestV1(t *testing.T) {
+	res := SolveV1(testCase1)
+	require.Equal(t, 0, res)
+}
+
+func TestV2(t *testing.T) {
+	res := SolveV2(testCase1)
+	require.Equal(t, 0, res)
+}
