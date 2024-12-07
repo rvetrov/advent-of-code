0bb0b0adbbde0327ca28869c1b8d4bdec05ff2bf
Roman <ravetrov@gmail.com>
2024-12-07 15:23:54 +0400

    2024/day07: solved

--- y2024/day07/main.go
+++ y2024/day07/main.go
@@ -1,9 +1,69 @@
 package day07
 
+import (
+	"strconv"
+	"strings"
+
+	"adventofcode.com/internal/utils"
+)
+
+func parseEquation(line string) (int, []int) {
+	parts := strings.Split(line, ":")
+	result, _ := strconv.Atoi(parts[0])
+	return result, utils.ReadNumbers(parts[1])
+}
+
+func canBeProducedByTwoOps(result int, nums []int) bool {
+	if len(nums) == 1 {
+		return result == nums[0]
+	}
+	last := nums[len(nums)-1]
+	if result%last == 0 && canBeProducedByTwoOps(result/last, nums[:len(nums)-1]) {
+		return true
+	}
+	return canBeProducedByTwoOps(result-last, nums[:len(nums)-1])
+}
+
+func canBeProducedByThreeOps(result int, nums []int) bool {
+	if len(nums) == 1 {
+		return result == nums[0]
+	}
+	last := nums[len(nums)-1]
+	if result%last == 0 && canBeProducedByThreeOps(result/last, nums[:len(nums)-1]) {
+		return true
+	}
+	if canBeProducedByThreeOps(result-last, nums[:len(nums)-1]) {
+		return true
+	}
+
+	for last > 0 && result > 0 {
+		if last%10 != result%10 {
+			return false
+		}
+		last /= 10
+		result /= 10
+	}
+	return canBeProducedByThreeOps(result, nums[:len(nums)-1])
+}
+
 func SolveV1(input string) int {
-	return 0
+	res := 0
+	for _, line := range utils.NonEmptyLines(input) {
+		eqRes, nums := parseEquation(line)
+		if canBeProducedByTwoOps(eqRes, nums) {
+			res += eqRes
+		}
+	}
+	return res
 }
 
 func SolveV2(input string) int {
-	return 0
+	res := 0
+	for _, line := range utils.NonEmptyLines(input) {
+		eqRes, nums := parseEquation(line)
+		if canBeProducedByThreeOps(eqRes, nums) {
+			res += eqRes
+		}
+	}
+	return res
 }
--- y2024/day07/main_test.go
+++ y2024/day07/main_test.go
@@ -7,15 +7,25 @@ import (
 )
 
 const (
-	testCase0 = ``
+	testCase0 = `
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
 )
 
 func TestSolveV1(t *testing.T) {
 	res := SolveV1(testCase0)
-	require.Equal(t, 0, res)
+	require.Equal(t, 3749, res)
 }
 
 func TestSolveV2(t *testing.T) {
 	res := SolveV2(testCase0)
-	require.Equal(t, 0, res)
+	require.Equal(t, 11387, res)
 }
