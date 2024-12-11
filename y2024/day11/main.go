cf2756b082567c7a8ea9dd7c3981319b7b10b135
Roman <ravetrov@gmail.com>
2024-12-11 23:52:45 +0400

    2024/day11: solved

--- y2024/day11/main.go
+++ y2024/day11/main.go
@@ -1,9 +1,61 @@
 package day11
 
+import (
+	"fmt"
+	"strconv"
+
+	"adventofcode.com/internal/utils"
+)
+
+func readStones(input string) map[int]int {
+	res := make(map[int]int)
+	for _, stone := range utils.ReadNumbers(utils.NonEmptyLines(input)[0]) {
+		res[stone]++
+	}
+	return res
+}
+
+func countStones(stones map[int]int) int {
+	res := 0
+	for _, value := range stones {
+		res += value
+	}
+	return res
+}
+
+func blink(stones map[int]int) map[int]int {
+	res := make(map[int]int)
+	for stone, cnt := range stones {
+		if stone == 0 {
+			res[1] += cnt
+			continue
+		}
+		numStr := fmt.Sprint(stone)
+		if len(numStr)%2 == 0 {
+			half := len(numStr) / 2
+			left, _ := strconv.Atoi(numStr[:half])
+			right, _ := strconv.Atoi(numStr[half:])
+			res[left] += cnt
+			res[right] += cnt
+		} else {
+			res[stone*2024] += cnt
+		}
+	}
+	return res
+}
+
+func solve(input string, blinks int) int {
+	stones := readStones(input)
+	for range blinks {
+		stones = blink(stones)
+	}
+	return countStones(stones)
+}
+
 func SolveV1(input string) int {
-	return 0
+	return solve(input, 25)
 }
 
 func SolveV2(input string) int {
-	return 0
+	return solve(input, 75)
 }
--- y2024/day11/main_test.go
+++ y2024/day11/main_test.go
@@ -7,15 +7,38 @@ import (
 )
 
 const (
-	testCase0 = ``
+	testCase0 = `125 17`
 )
+
+func TestBlink(t *testing.T) {
+	for _, tc := range []struct {
+		chain []string
+	}{
+		{[]string{
+			"0 1 10 99 999",
+			"1 2024 1 0 9 9 2021976",
+		}},
+		{[]string{
+			"125 17",
+			"253000 1 7",
+			"253 0 2024 14168",
+			"512072 1 20 24 28676032",
+			"512 72 2024 2 0 2 4 2867 6032",
+			"1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32",
+			"2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2",
+		}},
+	} {
+		for i := 1; i < len(tc.chain); i++ {
+			before := readStones(tc.chain[i-1])
+			after := blink(before)
+			expected := readStones(tc.chain[i])
+
+			require.Equal(t, expected, after)
+		}
+	}
+}
 
 func TestSolveV1(t *testing.T) {
 	res := SolveV1(testCase0)
-	require.Equal(t, 0, res)
-}
-
-func TestSolveV2(t *testing.T) {
-	res := SolveV2(testCase0)
-	require.Equal(t, 0, res)
+	require.Equal(t, 55312, res)
 }
