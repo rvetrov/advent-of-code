b79e61d792e404f55acaa20adc51dd8b4cfb0166
Roman <ravetrov@gmail.com>
2024-12-12 22:41:30 +0400

    2024/day12: solved

--- y2024/day12/main.go
+++ y2024/day12/main.go
@@ -1,9 +1,59 @@
 package day12
 
+import (
+	"adventofcode.com/internal/grid"
+	"adventofcode.com/internal/utils"
+)
+
+func walk(pos grid.Position, gr grid.Grid, visited map[grid.Position]struct{}) (area, perimeter, sides int) {
+	area++
+	visited[pos] = struct{}{}
+	curCh, _ := gr.At(pos)
+	for _, dir := range grid.FourSides {
+		nextPos := pos.Add(dir)
+		if ch, inside := gr.At(nextPos); inside && ch == curCh {
+			if _, v := visited[nextPos]; !v {
+				nextArea, nextPerimeter, newSides := walk(nextPos, gr, visited)
+				area += nextArea
+				perimeter += nextPerimeter
+				sides += newSides
+			}
+		} else {
+			perimeter++
+			nextToCur := pos.Add(dir.TurnCW())
+			nextToNext := nextPos.Add(dir.TurnCW())
+			nextToCurCh, nextToCurInside := gr.At(nextToCur)
+			nextToNextCh, nextToNextInside := gr.At(nextToNext)
+			if !(nextToCurInside && nextToCurCh == curCh) || (nextToNextInside && nextToNextCh == curCh) {
+				sides++
+			}
+		}
+	}
+	return area, perimeter, sides
+}
+
 func SolveV1(input string) int {
-	return 0
+	gr := grid.New(utils.NonEmptyLines(input))
+	visited := make(map[grid.Position]struct{})
+	res := 0
+	for pos := gr.Start(); gr.Contains(pos); pos = gr.Next(pos) {
+		if _, v := visited[pos]; !v {
+			area, perimeter, _ := walk(pos, gr, visited)
+			res += area * perimeter
+		}
+	}
+	return res
 }
 
 func SolveV2(input string) int {
-	return 0
+	gr := grid.New(utils.NonEmptyLines(input))
+	visited := make(map[grid.Position]struct{})
+	res := 0
+	for pos := gr.Start(); gr.Contains(pos); pos = gr.Next(pos) {
+		if _, v := visited[pos]; !v {
+			area, _, sides := walk(pos, gr, visited)
+			res += area * sides
+		}
+	}
+	return res
 }
--- y2024/day12/main_test.go
+++ y2024/day12/main_test.go
@@ -7,15 +7,64 @@ import (
 )
 
 const (
-	testCase0 = ``
+	testCase0 = `
+AAAA
+BBCD
+BBCC
+EEEC
+`
+	testCase1 = `
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+`
+	testCase2 = `
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+`
+	testCase3 = `
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+`
+	testCase4 = `
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+`
 )
 
 func TestSolveV1(t *testing.T) {
 	res := SolveV1(testCase0)
-	require.Equal(t, 0, res)
+	require.Equal(t, 140, res)
+	res = SolveV1(testCase1)
+	require.Equal(t, 1930, res)
 }
 
 func TestSolveV2(t *testing.T) {
 	res := SolveV2(testCase0)
-	require.Equal(t, 0, res)
+	require.Equal(t, 80, res)
+	res = SolveV2(testCase1)
+	require.Equal(t, 1206, res)
+	res = SolveV2(testCase2)
+	require.Equal(t, 436, res)
+	res = SolveV2(testCase3)
+	require.Equal(t, 236, res)
+	res = SolveV2(testCase4)
+	require.Equal(t, 368, res)
 }
