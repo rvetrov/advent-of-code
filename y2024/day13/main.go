57c602c7ccd51f743ed76f7666664992bdb99f0e
Roman <ravetrov@gmail.com>
2024-12-15 22:00:24 +0400

    2024/day13: solved

--- y2024/day13/main.go
+++ y2024/day13/main.go
@@ -1,9 +1,104 @@
 package day13
 
+import (
+	"fmt"
+	"strings"
+
+	"adventofcode.com/internal/grid"
+	"adventofcode.com/internal/math"
+	"adventofcode.com/internal/utils"
+)
+
+func parseVec(line, formatStr string) grid.Direction {
+	_, vecStr, _ := strings.Cut(line, ": ")
+	x, y := 0, 0
+	if read, err := fmt.Sscanf(vecStr, formatStr, &x, &y); read != 2 || err != nil {
+		panic(vecStr)
+	}
+	return grid.Direction{DR: y, DC: x}
+}
+
+func parseInput(lines []string) (a, b, target grid.Direction) {
+	a = parseVec(lines[0], "X+%d, Y+%d")
+	b = parseVec(lines[1], "X+%d, Y+%d")
+	target = parseVec(lines[2], "X=%d, Y=%d")
+	return a, b, target
+}
+
+func bruteForceSolve(a, b, target grid.Direction) int {
+	res := 0
+	for ai := range 101 {
+		for bi := range 101 {
+			cost := ai*3 + bi
+			if (res == 0 || res > cost) && a.Multiplied(ai).Add(b.Multiplied(bi)) == target {
+				res = cost
+			}
+		}
+	}
+	return res
+}
+
+func solve(a, b, target grid.Direction) int {
+	/*
+		a.DC * m + b.DC * n = target.DC
+		a.DR * m + b.DR * n = target.DR
+
+		a1*m + a2*n = a3
+		b1*m + b2*n = b3
+	*/
+	m, n := 0, 0
+
+	a1, a2, a3 := a.DC, b.DC, target.DC
+	b1, b2, b3 := a.DR, b.DR, target.DR
+	lcm := math.LCM(a1, b1)
+
+	k1 := lcm / a1
+	a1 *= k1
+	a2 *= k1
+	a3 *= k1
+
+	k2 := lcm / b1
+	b1 *= k2
+	b2 *= k2
+	b3 *= k2
+
+	b1 -= a1
+	b2 -= a2
+	b3 -= a3
+
+	if b3%b2 != 0 {
+		return 0
+	}
+	n = b3 / b2
+
+	a3 -= a2 * n
+	if a3%a1 != 0 {
+		return 0
+	}
+	m = a3 / a1
+
+	if m < 0 || n < 0 {
+		return 0
+	}
+	return m*3 + n
+}
+
 func SolveV1(input string) int {
-	return 0
+	res := 0
+	for _, block := range utils.SplitByEmptyLine(input) {
+		a, b, target := parseInput(block)
+		res += bruteForceSolve(a, b, target)
+	}
+	return res
 }
 
 func SolveV2(input string) int {
-	return 0
+	res := 0
+	for _, block := range utils.SplitByEmptyLine(input) {
+		a, b, target := parseInput(block)
+		target.DC += 10000000000000
+		target.DR += 10000000000000
+		res += solve(a, b, target)
+	}
+	return res
 }
--- y2024/day13/main_test.go
+++ y2024/day13/main_test.go
@@ -1,21 +1,84 @@
 package day13
 
 import (
-	"testing"
-
+	"adventofcode.com/internal/utils"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
-const (
-	testCase0 = ``
+var (
+	testCases = []struct {
+		line   string
+		result int
+	}{
+		{
+			`
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400`,
+			280,
+		},
+		{
+			`
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176`,
+			0,
+		},
+		{
+			`
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450`,
+			200,
+		},
+		{
+			`
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`,
+			0,
+		},
+		{
+			`
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400`,
+			0,
+		},
+		{
+			`
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=10000000012748, Y=10000000012176`,
+			459236326669,
+		},
+		{
+			`Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=10000000007870, Y=10000000006450`,
+			0,
+		},
+		{
+			`Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=10000000018641, Y=10000000010279`,
+			416082282239,
+		},
+	}
 )
 
 func TestSolveV1(t *testing.T) {
-	res := SolveV1(testCase0)
-	require.Equal(t, 0, res)
+	for _, tc := range testCases[:4] {
+		res := SolveV1(tc.line)
+		require.Equal(t, tc.result, res)
+	}
 }
 
 func TestSolveV2(t *testing.T) {
-	res := SolveV2(testCase0)
-	require.Equal(t, 0, res)
+	for _, tc := range testCases {
+		a, b, target := parseInput(utils.NonEmptyLines(tc.line))
+		res := solve(a, b, target)
+		require.Equal(t, tc.result, res)
+	}
 }
